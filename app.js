dc06e7c1ec77b1cb4a95d6a7b396a323497bb994
IQadmin123 <124127173+IQadmin123@users.noreply.github.com>
2023-03-31 17:59:18 +0530

    Update app.js

--- app.js
+++ app.js
@@ -88,7 +88,7 @@ app.get('/todo', function (req, res) {
 
     .listen(port, function () {
         // Logging to console
-        console.log(`Todolist running on http://0.0.0.0:${port}`)
+        console.log(`Todolist running on http://35.155.168.204:${port}`)
     });
 // Export app
 module.exports = app;
